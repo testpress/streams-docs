eb85a4b46a77523f0e7a8c02f5a8afaf1dfcf558
hari-testpress <hari@testpress.in>
2022-11-25 00:19:42 +0530

    Setup basic doc site

---
+++ docusaurus.config.js
@@ -0,0 +1,77 @@
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Streams Docs',
+  tagline: '',
+  url: 'https://testpress.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/testpress_favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'testpress', // Usually your GitHub org/user name.
+  projectName: 'streams-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  plugins: ['@docusaurus/theme-live-codeblock'],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Streams',
+        logo: {
+          alt: 'Testpress Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            position: 'left',
+            docId: 'server-api/organizations',
+            label: 'Server APIs',
+          },
+          {
+            href: 'https://github.com/testpress/streams-docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Testpress Tech labs`,
+      },
+      prism: {
+        theme: require('prism-react-renderer/themes/dracula'),
+        additionalLanguages: ['java', "php", "ruby", "csharp", "go", "kotlin"],
+      },
+      liveCodeBlock: {
+        playgroundPosition: 'bottom',
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Video DRM Encryption',
+    Svg: require('@site/static/img/security-svgrepo-com.svg').default,
+    description: (
+      <>
+        No downloader, plugin or hack can download the videos streamed through Testpress.
+      </>
+    ),
+  },
+  {
+    title: 'Dynamic Watermarking',
+    Svg: require('@site/static/img/web-security-svgrepo-com.svg').default,
+    description: (
+      <>
+        Total Screen Capture Block in Mobile Apps. Discourage screen capture in websites with user based watermark like IP address, user id, email id, phone no. etc.
+      </>
+    ),
+  },
+  {
+    title: 'Upload & Embed on Your Site Or App',
+    Svg: require('@site/static/img/app-plugin-plug-addon-connect-svgrepo-com.svg').default,
+    description: (
+      <>
+        Embed player with subtitles, multiple qualities & speed change options.
+        Option for the viewer to download securely in mobile app and play offline.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
